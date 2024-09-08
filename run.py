d7641b6b2ea027a6e9709efc60d8077ffecc7121
Eszter Kiskarpati <keszi94@gmail.com>
2024-09-08 20:58:56 +0000

    Create function welcome_message to display the game rules, style it using colorama

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import re
+from tabulate import tabulate
+from colorama import Fore, Back, Style, init
+
+# Initialize colorama for colored text
+init()
+
+# The welcome message printed at the top of the console
+def welcome_message():
+    print(Fore.MAGENTA + 'WELCOME TO BATTLESHIP!\n' + Style.RESET_ALL)
+    print('Rules:\n')
+    print('1.: The boards are ' + Fore.MAGENTA + '5x5 ' + Style.RESET_ALL + ', they start from ' + Fore.MAGENTA + 'row 1, column 1.' + Style.RESET_ALL)
+    print('2.: Each player has ' + Fore.MAGENTA + '4 ships' + Style.RESET_ALL + ' on their board.') 
+    print('3.: Try to sink the computers ships before it sinks yours.\n')
+    print(Fore.MAGENTA + 'Good luck!\n' + Style.RESET_ALL)   
+
+welcome_message()
