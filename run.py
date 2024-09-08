f67a78f9033f498783e15c020117534208180a49
Eszter Kiskarpati <keszi94@gmail.com>
2024-09-08 22:51:26 +0000

    Add player_game function, include isalpha() so input field only takes letters.

--- run.py
+++ run.py
@@ -6,13 +6,28 @@ from colorama import Fore, Back, Style, init
 # Initialize colorama for colored text
 init()
 
-# The welcome message printed at the top of the console
+# The welcome message and game rules printed at the top of the console
 def welcome_message():
-    print(Fore.MAGENTA + 'WELCOME TO BATTLESHIP!\n' + Style.RESET_ALL)
+    print()
+    print(Fore.MAGENTA + '              WELCOME TO BATTLESHIPS!\n' + Style.RESET_ALL)
     print('Rules:\n')
     print('1.: The boards are ' + Fore.MAGENTA + '5x5 ' + Style.RESET_ALL + ', they start from ' + Fore.MAGENTA + 'row 1, column 1.' + Style.RESET_ALL)
     print('2.: Each player has ' + Fore.MAGENTA + '4 ships' + Style.RESET_ALL + ' on their board.') 
-    print('3.: Try to sink the computers ships before it sinks yours.\n')
+    print("3.: Try to sink the computer's ships before it sinks yours.\n")
     print(Fore.MAGENTA + 'Good luck!\n' + Style.RESET_ALL)   
 
+def player_name():
+    """
+    An inmput field for theplayer to type in their name.
+    After the name is given and the player presse enter the game starts.
+    Using isalpha() so that the input field only takes letters. 
+    """
+    
+    name = input('Please input your name (no numbers, 10 characters max): ')
+    if len(name) <= 10 and name.isalpha():
+        return name
+    else :
+            print(Back.RED + 'Invalid input! Please try again! (no numbers, 10 characters max)' + Style.RESET_ALL)
+
 welcome_message()
+player_name()
