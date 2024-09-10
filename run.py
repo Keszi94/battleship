e0492fefaac66b7538b00d5b900403e306883afa
Eszter Kiskarpati <keszi94@gmail.com>
2024-09-10 15:54:58 +0000

    Modify code by breakng up lines that were too long

--- run.py
+++ run.py
@@ -4,86 +4,120 @@ from tabulate import tabulate
 from colorama import Fore, Back, Style, init
 
 # Initialize colorama for colored text
-init()
+init(autoreset=True)
 
-# The welcome message and game rules printed at the top of the console
+"""Declaring the player and computer scores globally"""
+player_score = 0
+computer_score = 0
+
+
 def welcome_message():
 
-    """ 
-    The game will start after the correct name is typed in and the player hits enter.
+    """
+    The game will start after the correct name is
+    typed in and the player hits enter.
     """
     print()
-    print(Fore.MAGENTA + 'WELCOME TO BATTLESHIPS!\n' + Style.RESET_ALL)
+    print(Fore.MAGENTA + 'WELCOME TO BATTLESHIPS!\n')
     print('Rules:\n')
-    print('1.: The boards are ' + Fore.MAGENTA + '5x5 ' + Style.RESET_ALL + ', they start from ' + Fore.MAGENTA + 'row 1, column 1.' + Style.RESET_ALL)
-    print('2.: Each player has ' + Fore.MAGENTA + '4 ships' + Style.RESET_ALL + ' on their board.') 
+    print('1.: The boards are ' + Fore.MAGENTA + '5x5' + Style.RESET_ALL +
+          ', they start from' + Fore.MAGENTA + 'row 1, column 1.')
+    print('2.: Each player has ' + Fore.MAGENTA + '4 ships' + Style.RESET_ALL +
+          ' on their board.')
     print("3.: Try to sink the computer's ships before it sinks yours.")
-    print("4.: Hits are marked with " + Fore.MAGENTA + "'X'" + Style.RESET_ALL + ", misses will display with an " + Fore.MAGENTA + "'O'" + Style.RESET_ALL + ".")
-    print("5.: You can see your ships on your board marked " + Fore.MAGENTA + "'S'" + Style.RESET_ALL + ".")
-    print("6.: You will have to choose a row(1-5) and a column(1-5) in each round.\n")
-    print(Fore.MAGENTA + 'Good luck!\n' + Style.RESET_ALL)   
+    print("4.: Hits are marked with " + Fore.MAGENTA +
+          "'X'" + Style.RESET_ALL +
+          ", misses will display with an " + Fore.MAGENTA + "'O'.")
+    print("5.: You can see your ships on your board marked " + Fore.MAGENTA +
+          "'S'.")
+    print("6.: You will have to choose a row(1-5) and a column(1-5) in \
+           each round.\n")
+    print(Fore.MAGENTA + 'Good luck!\n')
+
 
 def player_name_input():
     """
     An inmput field for the player to type in their name.
     After the name is given and the player pressed enter the game starts.
-    Using isalpha() so that the input field only takes letters. 
+    Using isalpha() so that the input field only takes letters.
     """
-    
+
     name = input('Please input your name (no numbers, 10 characters max): ')
     if len(name) <= 10 and name.isalpha():
         return name
-    else :
-            print(Back.RED + 'Invalid input! Please try again! (no numbers, 10 characters max)' + Style.RESET_ALL + '\n')
-            input('Please input your name (no numbers, 10 characters max): ')
+    else:
+        print(Back.RED + 'Invalid input! Please try again! (no numbers, 10' +
+              'characters max)' + Style.RESET_ALL + '\n')
+        input('Please input your name (no numbers, 10 characters max): ')
+
 
 class Boards:
     """
-    Creating the dimensions for the playing boards 
+    Creating the dimensions for the playing boards
     Placing the ships on the boards in random positions
     """
-    
+
     def __init__(self, size=5, num_ships=4):
-        self.size = size #default is 5
-        self.num_ships = num_ships #default is 4
+        self.size = size  # default is 5
+        self.num_ships = num_ships  # default is 4
         self.board = [[' ' for _ in range(size)] for _ in range(size)]
-        self.place_ships() #this will call the function that puts the ships into random cells on the board
-
-def place_ships(self):
-    """
-    Randomly place the 4 ships on the boards 
-    """
-    ships_placed = 0
-    while ships_placed < self.num_ships:
         """
-        Generating two random numbers between 0 and 4 for row(x) and column(y) 
+        This will call the function that puts the ships into
+        random cells on the board
         """
-        x = random.randint(0, self.size -1)
-        y = random.randint(0, self.size -1)
-        if self.board[x][y] == ' ': #checks if the selected position is empty
-            self.board[x][y] = 'S' #if the position is empty an 'S' is placed for 'Ship'
-            ships_placed += 1           
+        self.place_ships()
 
-def display_boards(self, board_title, show_ships_pos=False):
-    """
-    This function will display the boards
-    On the player's board the position of the ships will be visible 
-    """ 
-    display = [row[:] for row in self.board]
-    if not show_ships_pos:
-        display = [[' ' if cell == 'S' else cell for cell in row] for row in display]
-    
-    print(f"\n{board_title}") #prints whose board it is above the board (player's board, computer's board)
-    print(tabulate(display, tablfmt="fancy_grid", stralign="center")) #prints the boards
+    def place_ships(self):
+        """
+        Randomly place the 4 ships on the boards
+        """
+        ships_placed = 0
+        while ships_placed < self.num_ships:
+            """
+            Generating two random numbers between
+            0 and 4 for row(x) and column(y)
+            """
+            x = random.randint(0, self.size - 1)
+            y = random.randint(0, self.size - 1)
+            """Checks if the selected position is empty"""
+            if self.board[x][y] == ' ':
+                """If the position is empty an 'S' is placed for 'Ship'"""
+                self.board[x][y] = 'S'
+                ships_placed += 1
+
+    def display_boards(self, board_title, show_ships_pos=False):
+        """
+        This function will display the boards
+        On the player's board the position of the ships will be visible
+        """
+        display = [row[:] for row in self.board]
+        if not show_ships_pos:
+            display = [[' ' if cell == 'S' else cell for cell in row] for
+                       row in display]
+
+        """Prints whose board it is above the board (player's board,
+            computer's board)"""
+        print(f"\n{board_title}")
+        """Prints the boards"""
+        print(tabulate(display, tablefmt="double_grid", stralign="center"))
+
 
 def play_game():
-    """ 
+    """
     Runs all the program functions
 
     """
     welcome_message()
     player_name_input()
 
+# Display the player's and computer's board
+    player_board = Boards()
+    computer_board = Boards()
+# Put the titles above the boards
+    player_board.display_boards(board_title="Player's Board:",
+                                show_ships_pos=True)
+    computer_board.display_boards(board_title="Computer's Board:",
+                                  show_ships_pos=False)
+
 
 play_game()
-
