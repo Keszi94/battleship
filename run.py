0e1aad33af8944c7df3c1298efa5a8cc21c48871
Eszter Kiskarpati <keszi94@gmail.com>
2024-09-10 20:30:53 +0000

    Add logic to player_guess to inform player about repeat guess. Update play_game funtion.

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ def welcome_message():
     print("5.: You can see your ships on your board marked " + Fore.MAGENTA +
           "'S'.")
     print("6.: You will have to choose a row(1-5) and a column(1-5) in" +
-           " each round.\n")
+          " each round.\n")
     print(Fore.MAGENTA + 'Good luck!\n')
 
 
@@ -98,7 +98,7 @@ class Boards:
         """Prints whose board it is above the board (player's board,
             computer's board)"""
         print(f"\n{board_title}")
-        
+
         """Top border of the boards"""
         print("  " + "  ".join(["--" for _ in range(self.size)]))
 
@@ -113,7 +113,9 @@ class Boards:
  Print out if the player had a hit/miss at the en of the round
  Print out what the player's choice was
 """
-def player_guess(player_board, computer_board):
+
+
+def player_guess(player_board, computer_board, player_guesses):
     global player_score
     while True:
         try:
@@ -121,24 +123,33 @@ def player_guess(player_board, computer_board):
             col = int(input("Choose a column (1-5): ")) - 1
             if row not in range(5) or col not in range(5):
                 raise ValueError("You must choose a number between 1 and 5!")
+
+            # Check if coordinates have already been guessed
+            if (row, col) in player_guesses:
+                print(Back.YELLOW + Fore.BLACK + f"You already guessed " +
+                      f"row {row+1}, column {col+1}! Guess again!")
+                continue
+
+            # Add guess to previous guesses
+            player_guesses.add((row, col))
             break
         except ValueError as e:
             print(f"Invalid input! {e}")
 
-    print(f"Your guess was: " + Fore.MAGENTA + f"row {row + 1} column {col + 1}.\n") 
-    if computer_board.board[row][col] == 'S' :
+    print(f"Your guess was: " + Fore.MAGENTA +
+          f"row {row + 1} column {col + 1}.\n")
+    
+    if computer_board.board[row][col] == 'S':
         print(Fore.GREEN + "You hit a ship on the computer's board!\n")
         player_score += 1
         computer_board.board[row][col] = 'X'
     else:
         print(Fore.YELLOW + "You missed!\n")
-        computer_board.board[row][col] = 'O'           
+        computer_board.board[row][col] = 'O'
 
 
 def computer_guess(player_board):
     global computer_score
-
-
 
 
 def play_game():
@@ -147,29 +158,36 @@ def play_game():
 
     """
     welcome_message()
-    player_name_input()
-
-# Display the player's and computer's board
-    player_board = Boards()
-    computer_board = Boards()
+    player_name = player_name_input()
 
     while True:
 
-# Put the titles above the boards
-        player_board.display_boards(board_title="Player's Board:",
-                                    show_ships_pos=True)
-        computer_board.display_boards(board_title="Computer's Board:",
-                                      show_ships_pos=False)
+        # Display the player's and computer's board
+        player_board = Boards()
+        computer_board = Boards()
+        # Initialize guesses
+        player_guesses = set()
+        
+        
+        # Put the titles above the boards
+        while player_score < 4 and computer_score < 4:
+            player_board.display_boards(board_title="Player's Board:",
+                                        show_ships_pos=True)
+            computer_board.display_boards(board_title="Computer's Board:",
+                                          show_ships_pos=False)
+
+            player_guess(player_board, computer_board, player_guesses)
+            computer_guess(player_board)
 
         """
         Displays the player/computer guesses
         Checks if there is a final winner
         """
-        player_guess(player_board, computer_board) 
+        player_guess(player_board, computer_board)
 
         if player_score == 4:
-            print(Fore.GREEN + f"Congratulations {player_name}! You sank all" + 
-                   "the ships!")
+            print(Fore.GREEN + f"Congratulations {player_name}! You sank all" +
+                  "the ships!")
             break
 
         computer_guess(player_board)
@@ -178,5 +196,5 @@ def play_game():
             print(Fore.RED + f"Sorry, {player_name}! The computer" +
                   "sank all your ships! Better luck next time!")
 
-                 
+
 play_game()
